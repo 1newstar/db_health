eace6b63c31c55b644f4455208c58db9b3e1968b
liupzmin <676333678@qq.com>
2016-10-24 17:14:39 +0800

    修正dba_free_space中不显示100%的数据文件的问题
    使用左外联结修正dba_free_space中不显示100%的数据文件的问题

--- script/datafile_used.sql
+++ script/datafile_used.sql
@@ -1,16 +1,17 @@
-select a.tablespace_name,                                                                                                                                                                                                 
-       a.file_name,                                                                                                                                                                                                       
+select a.tablespace_name,
+       a.file_name,
        a.file_id,
-       a.relative_fno,                                                                                                                                                                                                    
+       a.relative_fno,
        a.AUTOEXTENSIBLE,
-       a.MAXBYTES,                                                                                                                                                                                                        
+       a.MAXBYTES,
        a.bytes / 1024 / 1024 "TOTAL(MB)",
-       round((a.bytes / 1024 / 1024) - (b.sb / 1024 / 1024), 2) "USED(MB)",                                                                                                                                               
-       round(b.sb / 1024 / 1024, 2) "FREE(MB)",
-       round(100 - (100 * b.sb / a.bytes), 2) "USED%",                                                                                                                                                                    
-       round(100 * b.sb / a.bytes, 2) "FREE%",
-       a.status                                                                                                                                                                                                           
+       nvl(round((a.bytes / 1024 / 1024) - (b.sb / 1024 / 1024), 2),
+           round(a.bytes / 1024 / 1024, 2)) "USED(MB)",
+       NVL(round(b.sb / 1024 / 1024, 2), 0) "FREE(MB)",
+       round(100 - (100 * nvl(b.sb, 0) / a.bytes), 2) "USED%",
+       round(100 * nvl(b.sb, 0) / a.bytes, 2) "FREE%",
+       a.status
   from dba_data_files a,
-       (select file_id, sum(BYTES) sb from dba_free_space group by file_id) b                                                                                                                                             
- where a.file_id = b.file_id                                                                                                                                                                                              
+       (select file_id, sum(BYTES) sb from dba_free_space group by file_id) b
+ where a.file_id = b.file_id(+)
  order by a.file_name;
