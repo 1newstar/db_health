f5e3ac5605d69151b1cd92db42f263a4b2a6562c
liupzmin <676333678@qq.com>
2017-01-09 10:20:44 +0800

    range分区检查按用户显示
    range分区检查按用户显示

--- script/range_partition_check.sql
+++ script/range_partition_check.sql
@@ -1,6 +1,8 @@
-select table_name,
+select table_owner,
+       table_name,
        count(1) "partition_count",
        max(substr(partition_name, 2)) "max_range_partition"
   from dba_tab_partitions
- where TABLE_OWNER = &special_users
- group by table_name;
+ where TABLE_OWNER in (&special_users)
+ group by table_owner, table_name
+ order by table_owner, table_name;
